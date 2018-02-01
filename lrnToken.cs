376f68b1aa57075e66406d79f1e8e48794828a8c
autumn84 <autumn84@aliyun.com>
2018-02-01 11:16:44 +0800

    create lrn token

---
+++ lrnToken.cs
@@ -0,0 +1,186 @@
+﻿/*
+
+  Copyright 2018 Loopring Foundation.
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+*/
+using Neo.SmartContract.Framework;
+using Neo.SmartContract.Framework.Services.Neo;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Numerics;
+
+namespace Neo.SmartContract
+{
+    public class XXX : Framework.SmartContract
+    {
+        //Token Settings
+        public static string Name() => "XXX Token";
+        public static string Symbol() => "XXX";
+        public static readonly byte[] Owner = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        
+
+        public static byte Decimals() => 8;
+        private const ulong factor = 100000000; //decided by Decimals()
+
+        private const ulong total_amount = 1395076054 * factor; // total token amount
+
+        private static Dictionary<byte[], Dictionary<byte[], BigInteger>> allowed = new Dictionary<byte[], Dictionary<byte[], BigInteger>>();
+
+        [DisplayName("transfer")]
+        public static event Action<byte[], byte[], BigInteger> Transferred;
+
+
+        public static Object Main(string operation, params object[] args)
+        {
+            if (Runtime.Trigger == TriggerType.Verification)
+            {
+                if (Owner.Length == 20)
+                {
+                    // if param Owner is script hash
+                    return Runtime.CheckWitness(Owner);
+                }
+                else if (Owner.Length == 33)
+                {
+                    // if param Owner is public key
+                    byte[] signature = operation.AsByteArray();
+                    return VerifySignature(signature, Owner);
+                }
+            }
+            else if (Runtime.Trigger == TriggerType.Application)
+            {
+                if (operation == "deploy") return Deploy();
+                if (operation == "totalSupply") return TotalSupply();
+                if (operation == "name") return Name();
+                if (operation == "symbol") return Symbol();
+                if (operation == "transfer")
+                {
+                    if (args.Length != 3) return false;
+                    byte[] from = (byte[])args[0];
+                    byte[] to = (byte[])args[1];
+                    BigInteger value = (BigInteger)args[2];
+                    return Transfer(from, to, value);
+                }
+                if (operation == "balanceOf")
+                {
+                    if (args.Length != 1) return 0;
+                    byte[] account = (byte[])args[0];
+                    return BalanceOf(account);
+                }
+                if (operation == "decimals") return Decimals();
+                if (operation == "allowance")
+                {
+                    return Allowance((byte[])args[0], (byte[])args[1]);
+                }
+                if (operation == "approve")
+                {
+                    return Approve((byte[])args[0], (byte[])args[1], (BigInteger)args[2]);
+                }
+                if (operation == "transferFrom")
+                {
+                    return TransferFrom((byte[])args[0], (byte[])args[1], (byte[])args[2], (BigInteger)args[3]);
+                }
+            }
+            return false;
+        }
+
+        // initialization parameters, only once
+        public static bool Deploy()
+        {
+            byte[] total_supply = Storage.Get(Storage.CurrentContext, "totalSupply");
+            if (total_supply.Length != 0) return false;
+            Storage.Put(Storage.CurrentContext, Owner, total_amount);
+            Storage.Put(Storage.CurrentContext, "totalSupply", total_amount);
+            Transferred(null, Owner, total_amount);
+            return true;
+        }
+
+        // get the total token supply
+        public static BigInteger TotalSupply()
+        {
+            return Storage.Get(Storage.CurrentContext, "totalSupply").AsBigInteger();
+        }
+
+        // function that is always called when someone wants to transfer tokens.
+        public static bool Transfer(byte[] from, byte[] to, BigInteger value)
+        {
+            if (value <= 0) return false;
+            if (!Runtime.CheckWitness(from)) return false;
+            if (from == to) return true;
+            BigInteger from_value = Storage.Get(Storage.CurrentContext, from).AsBigInteger();
+            if (from_value < value) return false;
+            if (from_value == value)
+                Storage.Delete(Storage.CurrentContext, from);
+            else
+                Storage.Put(Storage.CurrentContext, from, from_value - value);
+            BigInteger to_value = Storage.Get(Storage.CurrentContext, to).AsBigInteger();
+            Storage.Put(Storage.CurrentContext, to, to_value + value);
+            Transferred(from, to, value);
+            return true;
+        }
+
+        // get the account balance of another account with address
+        public static BigInteger BalanceOf(byte[] address)
+        {
+            return Storage.Get(Storage.CurrentContext, address).AsBigInteger();
+        }
+
+
+        // 检查可以转账的金额
+        public static BigInteger Allowance(byte[] from, byte[] to)
+        {
+            if(allowed.ContainsKey(from))
+            {
+                return allowed[from][to];
+            }
+            return 0;
+        }
+
+        // 从授权账户里转账
+        public static bool TransferFrom(byte[] originator, byte[] from, byte[] to, BigInteger amount)
+        {
+            BigInteger from_value = Storage.Get(Storage.CurrentContext, from).AsBigInteger();
+            // 处理金额不能超过授权金额
+            if (from_value < amount)
+            {
+                return false;
+            }
+            Storage.Put(Storage.CurrentContext, from, from_value - amount);
+            BigInteger to_value = Storage.Get(Storage.CurrentContext, to).AsBigInteger();
+            Storage.Put(Storage.CurrentContext, from, to_value + amount);
+
+            BigInteger allowance = allowed[originator][from];
+            allowed[originator][from] = allowance - amount;
+            Transfer(from, to, amount);
+            return true;
+
+        }
+
+        // 授权token
+        public static bool Approve(byte[] originator, byte[] to, BigInteger amount)
+        {
+            if ((amount != 0) && (allowed[originator][to] != 0))
+            {
+                return false;
+            }
+
+            allowed[originator][to] = amount;
+            //TODO 需要发送授权事件
+            return true;
+        }
+
+
+    }
+}
