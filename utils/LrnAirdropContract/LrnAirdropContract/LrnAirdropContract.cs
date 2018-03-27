87209912d55bae181bb90d0275b085243de03cde
autumn84 <autumn84@aliyun.com>
2018-03-27 12:47:40 +0800

    judge the excuting script while verification

--- utils/LrnAirdropContract/LrnAirdropContract/LrnAirdropContract.cs
+++ utils/LrnAirdropContract/LrnAirdropContract/LrnAirdropContract.cs
@@ -9,12 +9,12 @@ namespace LrnAirdropContract
     public class LrnAirdropContract : SmartContract
     {
         public static readonly byte[] SuperAdmin = "ASXA7rzm9hnrnhbReFEKVHknWpLnGhnN8T".ToScriptHash();
-        private static readonly byte[] neo_asset_id = { 155, 124, 255, 218, 166, 116, 190, 174, 15, 147, 14, 190, 96, 133, 175, 144, 147, 229, 254, 86, 179, 74, 92, 34, 12, 205, 207, 110, 252, 51, 111, 197 };
-        private const int AIRDROP_START_TIME = 88888888;
+        private static readonly byte[] GAS_ASSET_ID = { 231, 45, 40, 105, 121, 238, 108, 177, 183, 230, 93, 253, 223, 178, 227, 132, 16, 11, 141, 20, 142, 119, 88, 222, 66, 228, 22, 139, 113, 121, 44, 96 };
+        private static readonly byte INVOCATION_TRANSACTION_TYPE = 0xd1;
+        private const int AIRDROP_START_TIME = 1514736000;//2018-01-01 00:00:00
         private const string AIR_DROP_SUPPLY = "airdropSupply";
-        private const ulong RATE = 730;//two years
+        private const ulong PERIOD = 730;//two years
         private const int SECONDS_PER_DAY = 86400;
-        public delegate byte[] DynamicCall(string method, object[] arr);
 
         [Appcall("06c29b2661be437e9c38485f8797cb4c59ed5999")]
         static extern object CallLrn(string method, object[] arr);
@@ -34,26 +34,36 @@ namespace LrnAirdropContract
         {
             if (Runtime.Trigger == TriggerType.Verification)
             {
-                if (SuperAdmin.Length == 20)
-                {
-                    return Runtime.CheckWitness(SuperAdmin);
-                }
-
                 Transaction tx = (Transaction)ExecutionEngine.ScriptContainer;
+                var type = tx.Type;
                 var inputs = tx.GetInputs();
                 var outputs = tx.GetOutputs();
                 var attributes = tx.GetAttributes();
-                if(inputs.Length !=1 || outputs.Length != 1 || attributes.Length != 0)
+
+                if (type != INVOCATION_TRANSACTION_TYPE) return false;
+
+                if (inputs.Length != 1 || outputs.Length != 1 || attributes.Length != 0)
                 {
                     return false;
                 }
 
-                if(outputs[0].AssetId == neo_asset_id && outputs[0].Value == 1)
+                if (outputs[0].AssetId != GAS_ASSET_ID && outputs[0].Value != 1)
                 {
-                    return true;
+                    return false;
                 }
 
-                return false;
+                var invocationTransaction = (InvocationTransaction)tx;
+                if (invocationTransaction.Script.Length != 53)
+                {
+                    return false;
+                }
+
+                if (invocationTransaction.Script.Range(24,29) != "withdraw".AsByteArray().Concat(new byte[]{0x67}).Concat(ExecutionEngine.ExecutingScriptHash))
+                {
+                    return false;
+                }
+
+                return true;
             }
 
             if (Runtime.Trigger == TriggerType.Application)
@@ -144,20 +154,8 @@ namespace LrnAirdropContract
             int time = (int)now - AIRDROP_START_TIME;
             if (time < 0) return 0;
             int n = time / SECONDS_PER_DAY + 1;
-            BigInteger withdrawAmount = balance * n / RATE;
+            BigInteger withdrawAmount = balance * n / PERIOD;
             return withdrawAmount;
-        }
-
-        private static byte[] IntToBytes(BigInteger value)
-        {
-            byte[] buffer = value.ToByteArray();
-            return buffer;
-        }
-
-        private static BigInteger BytesToInt(byte[] array)
-        {
-            var buffer = new BigInteger(array);
-            return buffer;
         }
     }
 }
